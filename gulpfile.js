c6a17b1f910a5d5d79b8b9af8d06ff7be626cabe
Tomohiko Himura <eiel.hal@gmail.com>
2015-11-06 11:42:22 +0900

    stylusを設定

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,43 @@
 'use strict';
 
 var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var stylus = require('gulp-stylus');
 
-var server = require('./gulp/server');
 var webpack = require('./gulp/webpack');
 var copyHTML = require('./gulp/copy-html');
+
+var buildCSS = function() {
+  gulp.src('app/css/*.styl')
+    .pipe(stylus())
+    .pipe(gulp.dest('public/css/'))
+    .pipe(browserSync.stream());
+};
+
+var server = function() {
+    browserSync.init({
+        server: {
+            baseDir: "public"
+        }
+    });
+    gulp.watch("public/js/main.js").on('change', browserSync.reload);
+};
+
+var watch = function() {
+  gulp.watch("app/css/*", ['css']);
+};
+
 
 gulp.task('server', server);
 gulp.task('webpack', webpack);
 gulp.task('copy:html', copyHTML);
+gulp.task("css", buildCSS);
+gulp.task("watch", watch);
 
 gulp.task('default', [
   'webpack',
+  'css',
   'copy:html',
-  'server'
+  'server',
+  'watch'
   ]);
